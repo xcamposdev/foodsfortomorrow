2d8ca54fbe9134b076d2ace9fb9de7f78ed30b1b
xavi <xcampos@develoop.net>
2020-06-10 13:18:40 +0200

    [ADD] csv custom

---
+++ custom_modules/csv_custom/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Food For Tomorrow Custom',
+    'version': '1.0.0.0',
+    'author': 'Develoop Software S.A.',
+    'category': 'Food For Tomorrow',
+    'website': 'https://www.develoop.net/',
+    'depends': ['stock', 'base'],
+    'summary': 'Food For Tomorrow Extend Functionality',
+    'description': """
+        Permit search advanced in the importation
+        """,
+    'data': [
+    ],
+    'images': ['static/description/icon.png'],
+    'demo': [],
+    'installable': True,
+}
