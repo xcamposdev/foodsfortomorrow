36cffc7b9a0238be8bc7544ea7a654cfc6cebe2a
Andres <acastillo@develoop.net>
2020-06-30 19:53:00 -0400

    Fix

--- custom_modules/purchase_edicom_custom/models/purchase.py
+++ custom_modules/purchase_edicom_custom/models/purchase.py
@@ -9,53 +9,55 @@ import datetime
 
 _logger = logging.getLogger(__name__)
 
+
 class edicom_form(models.Model):
-    
+
     _inherit = 'purchase.order'
-    
+
     is_sent_edicom = fields.Boolean(default=False)
-    
+
     def send_by_edicom(self):
-        
+
         url = self.env['ir.config_parameter'].get_param('edicom_api_url')
-        username = self.env['ir.config_parameter'].get_param('edicom_api_username')
-        password = self.env['ir.config_parameter'].get_param('edicom_api_password')
-        
-        id_log = 0
-        
+        username = self.env['ir.config_parameter'].get_param(
+            'edicom_api_username')
+        password = self.env['ir.config_parameter'].get_param(
+            'edicom_api_password')
+
         sale_partner_id = ''
-        sale_order_origin = self.env['sale.order'].search([('name','=',self.origin)], limit=1)
+        sale_order_origin = self.env['sale.order'].search(
+            [('name', '=', self.origin)], limit=1)
         if(sale_order_origin):
             if(sale_order_origin.partner_id):
                 sale_partner_id = sale_order_origin.partner_id.x_studio_gln
-                             
+
         detail = []
         if(self.order_line):
             for order_line in self.order_line:
-                detail.append({ 
+                detail.append({
                     'clave1': self.name,
-                    'clave2': order_line.sequence or "", 
+                    'clave2': order_line.sequence or "",
                     'refean': order_line.product_id.x_studio_ean13 or "",
                     'dun14': order_line.product_id.x_studio_gtin14 or "",
                     'refetiq': order_line.product_id.x_studio_gtin14 or "",
                     'descmer': order_line.product_id.name or "",
                     'cantped': order_line.product_qty or "0",
-                    })
-        
+                })
+
         fecha = self.date_approve or ""
         fecha_solicitud_entrega = self.x_studio_fecha_solicitud_entrega or ""
-        
+
         current_register_log = self.env['x_orders_salida'].create({
-                'x_name': self.name,
-                'x_studio_fecha_ltimo_intento_1': datetime.datetime.now(),
-                'x_studio_descripcin_estado_1': "",
-                'x_studio_field_rGeUU': "status1"
-            })
+            'x_name': self.name,
+            'x_studio_fecha_ltimo_intento_1': datetime.datetime.now(),
+            'x_studio_descripcin_estado_1': "",
+            'x_studio_field_rGeUU': "status1"
+        })
 
         header = {
             'clave1': self.name,
             'nodo': 220,
-            'numped': self.partner_ref or "", 
+            'numped': self.partner_ref or "",
             'fecha': str(fecha),
             'fechaepr': str(fecha_solicitud_entrega),
             'fechatop': str(fecha_solicitud_entrega),
@@ -69,15 +71,15 @@ class edicom_form(models.Model):
             'details': detail,
             'id_log': current_register_log.id
         }
- 
-        response = requests.post(url+"/api/order-register", 
-            data=json.dumps(header), 
-            headers={'Content-type': 'application/json'}, 
-            auth=HTTPBasicAuth(username,password),
-            timeout=60)
-        
+
+        response = requests.post(url+"/api/order-register",
+                                 data=json.dumps(header),
+                                 headers={'Content-type': 'application/json'},
+                                 auth=HTTPBasicAuth(username, password),
+                                 timeout=60)
+
         _message = ""
-        
+
         if (response.status_code == 500):
             _message = "Ocurrio un error en el servidor."
             current_register_log.write({
@@ -90,24 +92,26 @@ class edicom_form(models.Model):
             _message = "Las credenciales de autenticidad son incorrectas."
         elif (response.status_code == 200):
             _message = "La solicitud de presupuesto se envio correctamente."
-            current_register_log.write({ 
+            current_register_log.write({
                 'x_name': self.name,
                 'x_studio_fecha_ltimo_intento_1': datetime.datetime.now(),
                 'x_studio_descripcin_estado_1': "Estado del pedido en proceso",
                 'x_studio_field_rGeUU': "status1"
             })
             self.is_sent_edicom = True
-            
-        new = self.env['purchase.edicom.modal.notification'].create({'msg':_message})
+
+        new = self.env['purchase.edicom.modal.notification'].create({
+                                                                    'msg': _message})
         return {
             'type': 'ir.actions.act_window',
             'name': 'Información',
             'res_model': 'purchase.edicom.modal.notification',
             'view_type': 'form',
             'view_mode': 'form',
-            'res_id'    : new.id,
+            'res_id': new.id,
             'target': 'new',
         }
+
 
 class edicom_form_notification_0(models.TransientModel):
     _name = "purchase.edicom.modal.notification"
