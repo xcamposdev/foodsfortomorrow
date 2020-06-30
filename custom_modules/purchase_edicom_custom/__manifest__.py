6139cd4238e419406c21371da4b9ea390cc4add7
Andres <acastillo@develoop.net>
2020-06-30 19:40:26 -0400

    Fix to send to edicom

---
+++ custom_modules/purchase_edicom_custom/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'FFT edicom purchase custom',
+    'version': '1.0.0.0',
+    'author': 'Develoop Software S.A.',
+    'category': 'Food For Tomorrow',
+    'website': 'https://www.develoop.net/',
+    'depends': ['purchase', 'base'],
+    'summary': 'Food For Tomorrow Extend Functionality',
+    'description': """
+        Implement integration with external api
+        """,
+    'data': [
+    	'views/purchase.xml',
+    ],
+    'images': ['static/description/icon.png'],
+    'demo': [],
+    'installable': True,
+}
