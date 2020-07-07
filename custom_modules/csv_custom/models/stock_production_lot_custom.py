cbcdb721b9970cfd7edb7c85de4860261c6e78b4
Andres <acastillo@develoop.net>
2020-07-07 00:59:51 -0400

    Signed-off-by: Andres <acastillo@develoop.net>

--- custom_modules/csv_custom/models/stock_production_lot_custom.py
+++ custom_modules/csv_custom/models/stock_production_lot_custom.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import logging
 from odoo import api, fields, models, _
+
+_logger = logging.getLogger(__name__)
 
 class stock_production_lot_custom_search(models.Model):
 
@@ -16,4 +19,13 @@ class stock_production_lot_custom_search(models.Model):
             stock_production_lot = self._search([('id', '=', name)] + args, limit=limit, access_rights_uid=name_get_uid)
             if not stock_production_lot:
                 stock_production_lot = self._search([('name', '=', name)] + args, limit=limit, access_rights_uid=name_get_uid)
-        return models.lazy_name_get(self.browse(stock_production_lot).with_user(name_get_uid))
+            return models.lazy_name_get(self.browse(stock_production_lot).with_user(name_get_uid))
+        
+        args = list(args or [])
+        if not self._rec_name:
+            _logger.warning("Cannot execute name_search, no _rec_name defined on %s", self._name)
+        elif not (name == '' and operator == 'ilike'):
+            args += [(self._rec_name, operator, name)]
+        ids = self._search(args, limit=limit, access_rights_uid=name_get_uid)
+        recs = self.browse(ids)
+        return models.lazy_name_get(recs.with_user(name_get_uid))
