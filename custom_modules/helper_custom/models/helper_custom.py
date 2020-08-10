ccd046b09ca4ceaea46dfb17a182a861ed54a397
Andres <acastillo@develoop.net>
2020-08-10 12:20:27 -0400

    Actualizar datos antiguos

--- custom_modules/helper_custom/models/helper_custom.py
+++ custom_modules/helper_custom/models/helper_custom.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+import logging
 from odoo import api, fields, models, exceptions, _
 from odoo.exceptions import AccessError, UserError, ValidationError
 
+_logger = logging.getLogger(__name__)
 
 class Update_Data_form(models.Model):
     
@@ -9,8 +11,11 @@ class Update_Data_form(models.Model):
 
     def update_data(self):
         all_sales = self.env['sale.order'].search([('id','>',0)])
+        count = 0
         for sale in all_sales:
             if(sale.analytic_account_id == False or sale.analytic_account_id.id == False):
                 if(sale.partner_id):
-                    sale.analytic_account_id = sale.partner_id.x_studio_canal_de_venta
-        raise exceptions.Warning("COMPLETADO") 
+                    sale.write({ 'analytic_account_id': sale.partner_id.x_studio_canal_de_venta.id })
+                    _logger.info("ACTUALIZADO %s", sale.name)
+                    count = count + 1
+        _logger.info("CANTIDAD TOTAL %s", count)
