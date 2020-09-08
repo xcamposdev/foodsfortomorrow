fe90a71559525b9477eb5e4adc465c9e66b4e670
Andres <acastillo@develoop.net>
2020-09-08 11:21:43 -0400

    Update orden de compra filtro proveedores

---
+++ custom_modules/purchase_order_custom/models/purchase_order_custom.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from itertools import groupby
+
+from odoo import api, fields, models, exceptions, SUPERUSER_ID, _
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools import float_is_zero, float_compare
+
+
+class PurchaseOrderCustom0(models.Model):
+
+    _inherit = 'purchase.order'
+
+    READONLY_STATES = {
+        'purchase': [('readonly', True)],
+        'done': [('readonly', True)],
+        'cancel': [('readonly', True)],
+    }
+
+    partner_id = fields.Many2one(
+        'res.partner', string='Vendor', required=True, states=READONLY_STATES, 
+        change_default=True, tracking=True, 
+        domain="['|','&', ('company_id', '=', False), ('company_id', '=', company_id), ('parent_id', '=', False)]", 
+        help="You can find a vendor by its Name, TIN, Email or Internal Reference.")
+    
+
+    # partner_id = fields.Many2one(
+    #     'res.partner', string='Customer', readonly=True,
+    #     states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
+    #     required=True, change_default=True, index=True, tracking=1,
+    #     domain="['|','&','|', ('company_id', '=', False), ('company_id', '=', company_id), ('parent_id', '=', False), ('type', '=', 'delivery')]",)
