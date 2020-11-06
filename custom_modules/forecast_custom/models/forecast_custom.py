10f27f0cdfb6f61e1b3d46ef203a91ad014bcb29
Andres <acastillo@develoop.net>
2020-11-06 12:14:30 -0400

    Modificaciones a calculos para forecast

--- custom_modules/forecast_custom/models/forecast_custom.py
+++ custom_modules/forecast_custom/models/forecast_custom.py
@@ -16,72 +16,68 @@ class ForecastSales(models.Model):
     _description = "forecast sale"
     _order = 'x_mes desc, x_producto asc, id desc'
 
-    x_cajas = fields.Integer(string="Cajas", default=0)
+    x_cajas = fields.Integer(string="Cajas")
     x_comercial = fields.Many2one('res.users', string="Comercial", readonly=True)
     x_contacto = fields.Many2one('res.partner', string="Contacto", readonly=True)
     x_cuenta_analitica = fields.Many2one('account.analytic.account', string="Cuenta analítica", readonly=True)
-    x_kg = fields.Float(string="Kg", default=0)
+    x_kg = fields.Float(string="Kg")
     x_mes = fields.Date(string="Mes", readonly=True)
     x_mes_format = fields.Char(string="Mes/Año", readonly=True, required=True)
     x_producto = fields.Many2one('product.product', string="Producto", readonly=True, required=True)
-    x_rotacion = fields.Float(string="Rotación", compute="x_rotacion_compute")
+    x_rotacion = fields.Float(string="Rotación", readonly=True)
     x_tipo = fields.Selection([
         ('cliente','Cliente'),
         ('canal','Canal')
         ], string="Tipo", readonly=True)
-    x_unidades = fields.Integer(string="Unidades", default=0)
+    x_unidades = fields.Integer(string="Unidades")
     x_locked = fields.Boolean("Bloqueado", default=False)
 
     @api.model
     def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
-        if(self._context.get('is_tree',False)):
-            args += [('x_comercial', '=', self.env.user.id)]
+        # if(self._context.get('is_tree',False)):
+        args += [('x_comercial', '=', self.env.user.id)]
         return super(ForecastSales, self)._search(args, offset=offset, limit=limit, order=order, count=count, access_rights_uid=access_rights_uid)
 
-    @api.onchange('x_unidades')
-    def x_unidades_onchange(self):
-        self.x_cajas = self.x_unidades * self.x_producto.x_studio_unidades_por_caja
-        self.x_kg = self.x_cajas * self.x_producto.x_studio_peso_umb_gr / 1000
 
-    @api.onchange('x_cajas')
-    def x_cajas_onchange(self):
-        self.x_unidades = self.x_cajas / (self.x_producto.x_studio_unidades_por_caja if self.x_producto.x_studio_unidades_por_caja > 0 else 1)
+    def onchange(self, values, field_name, field_onchange):
+        # OVERRIDE
+        # As the dynamic lines in this model are quite complex, we need to ensure some computations are done exactly
+        # at the beginning / at the end of the onchange mechanism. So, the onchange recursivity is disabled.
+        return super(ForecastSales, self.with_context(recursive_onchanges=False)).onchange(values, field_name, field_onchange)
+
+    @api.onchange('x_unidades')
+    def x_unidades_change(self):
+        self.x_cajas = self.x_unidades / self.x_producto.x_studio_unidades_por_caja
         self.x_kg = self.x_cajas * self.x_producto.x_studio_peso_umb_gr / 1000
+        
+        resto = self.x_unidades % self.x_producto.x_studio_unidades_por_caja
+        #recursive_onchanges
+        if(resto > 0):
+            return {
+                'warning':{
+                    'title': "Información: División inexacta",
+                    'message': "La división entre " + str(self.x_unidades) + " (unidades) y " + str(self.x_producto.x_studio_unidades_por_caja) + " (unidades por caja) genera un resto de " + str(resto)
+                }
+            }
+    
+    @api.onchange('x_cajas')
+    def x_cajas_change(self):
+        self.x_unidades = self.x_cajas * self.x_producto.x_studio_unidades_por_caja
+        self.x_kg = self.x_cajas * self.x_producto.x_studio_peso_umb_gr / 1000
+        
 
     @api.onchange('x_kg')
-    def x_kg_onchange(self):
+    def x_kg_change(self):
         self.x_cajas = self.x_kg * 1000 / (self.x_producto.x_studio_peso_umb_gr if self.x_producto.x_studio_peso_umb_gr > 0 else 1)
-        self.x_unidades = self.x_cajas / (self.x_producto.x_studio_unidades_por_caja if self.x_producto.x_studio_unidades_por_caja > 0 else 1)
-        
-    def x_rotacion_compute(self):
-        for record in self:
-            quantity_sale = 0
-            end_date = datetime.datetime(datetime.date.today().year,datetime.date.today().month,1)
-            start_date = end_date + relativedelta(months=-1)
-            
-            days_previous_month = (end_date + relativedelta(days=-1)).day
-            number_of_sundays = len([1 for i in calendar.monthcalendar(start_date.year, start_date.month) if i[6] != 0])
-
-            if(record.x_contacto):
-                quantity_record = self.env['sale.order.line'].search([('product_id','=',record.x_producto.id),\
-                        ('order_id.state','=','sale'),\
-                        ('order_id.partner_invoice_id','=',record.x_contacto.id),\
-                        ('order_id.date_order','>=',start_date),('order_id.date_order','<',end_date)])
-                for line in quantity_record:
-                    quantity_sale = quantity_sale + ((line.product_uom_qty * record.x_producto.x_studio_peso_umb_gr)/1000)
-                record['x_rotacion'] = quantity_sale / (days_previous_month - number_of_sundays) / (record.x_contacto.x_studio_puntos_de_venta_foods if record.x_contacto.x_studio_puntos_de_venta_foods > 0 else 1)
-                
-            elif(record.x_cuenta_analitica):
-                quantity_record = self.env['sale.order.line'].search([('product_id','=',record.x_producto.id),\
-                        ('order_id.state','=','sale'),\
-                        ('order_id.analytic_account_id','=',record.x_cuenta_analitica.id),\
-                        ('order_id.date_order','>=',start_date),('order_id.date_order','<',end_date)])
-                for line in quantity_record:
-                    quantity_sale = quantity_sale + ((line.product_uom_qty * record.x_producto.x_studio_peso_umb_gr)/1000)
-                record['x_rotacion'] = quantity_sale / (days_previous_month - number_of_sundays)
-                
-            else:
-                record['x_rotacion'] = 0
+        self.x_unidades = self.x_cajas * self.x_producto.x_studio_unidades_por_caja
+        resto = self.x_kg * 1000 % (self.x_producto.x_studio_peso_umb_gr if self.x_producto.x_studio_peso_umb_gr > 0 else 1)
+        if(resto > 0):
+            return {
+                'warning':{
+                    'title': "Información: División inexacta",
+                    'message': "La división entre " + str(self.x_kg * 1000) + " (gramos) y " + str(self.x_producto.x_studio_peso_umb_gr) + " (peso neto UMB gr) genera un resto de " + str(resto)
+                }
+            }
 
     def forecast_change_field_locked(self):
 
@@ -111,6 +107,6 @@ class ForecastCatalog(models.Model):
 
     @api.model
     def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
-        if(self._context.get('is_tree',False)):
-            args += [('x_comercial', '=', self.env.user.id)]
+        # if(self._context.get('is_tree',False)):
+        args += [('x_comercial', '=', self.env.user.id)]
         return super(ForecastCatalog, self)._search(args, offset=offset, limit=limit, order=order, count=count, access_rights_uid=access_rights_uid)
--- custom_modules/forecast_custom/wizard/forecast_wizard.py
+++ custom_modules/forecast_custom/wizard/forecast_wizard.py
@@ -33,11 +33,13 @@ class forecast_wizard_custom_0(models.TransientModel):
             for catalogo in data_catalogo:
                 date_format = (str(self.date_generate.month) if len(str(self.date_generate.month))==2 else "0" + str(self.date_generate.month)) + "/" + str(self.date_generate.year)
                 
+                rotacion = self.x_rotacion_compute(catalogo)
+
                 self.env['x.forecast.sale'].create({
                     'x_mes_format': date_format,
                     'x_mes': self.date_generate,
                     'x_producto': catalogo.x_producto.id,
-                    'x_rotacion': 0,
+                    'x_rotacion': rotacion,
                     'x_kg': 0,
                     'x_cajas': 0,
                     'x_tipo': catalogo.x_tipo,
@@ -48,7 +50,7 @@ class forecast_wizard_custom_0(models.TransientModel):
                 })
         
         return {
-            'name': "Forecast-ventas",
+            'name': "Ventas",
             'view_type': 'form',
             'view_mode': 'tree',
             'res_model': 'x.forecast.sale',
@@ -57,3 +59,32 @@ class forecast_wizard_custom_0(models.TransientModel):
             'target': 'main',
             'nodestroy': True
         }
+
+    def x_rotacion_compute(self, catalog):
+        quantity_sale = 0
+        end_date = datetime.datetime(datetime.date.today().year,datetime.date.today().month,1)
+        start_date = end_date + relativedelta(months=-1)
+        
+        days_previous_month = (end_date + relativedelta(days=-1)).day
+        number_of_sundays = len([1 for i in calendar.monthcalendar(start_date.year, start_date.month) if i[6] != 0])
+
+        if(catalog.x_contacto):
+            quantity_record = self.env['sale.order.line'].search([('product_id','=',catalog.x_producto.id),\
+                    ('order_id.state','=','sale'),\
+                    ('order_id.partner_invoice_id','=',catalog.x_contacto.id),\
+                    ('order_id.date_order','>=',start_date),('order_id.date_order','<',end_date)])
+            for line in quantity_record:
+                quantity_sale = quantity_sale + (line.product_uom_qty * (catalog.x_producto.x_studio_unidades_por_caja if catalog.x_producto.x_studio_unidades_por_caja > 0 else 1))
+            return quantity_sale / (days_previous_month - number_of_sundays) / (catalog.x_contacto.x_studio_puntos_de_venta_foods if catalog.x_contacto.x_studio_puntos_de_venta_foods > 0 else 1)
+            
+        elif(catalog.x_cuenta_analitica):
+            quantity_record = self.env['sale.order.line'].search([('product_id','=',catalog.x_producto.id),\
+                    ('order_id.state','=','sale'),\
+                    ('order_id.analytic_account_id','=',catalog.x_cuenta_analitica.id),\
+                    ('order_id.date_order','>=',start_date),('order_id.date_order','<',end_date)])
+            for line in quantity_record:
+                quantity_sale = quantity_sale + (line.product_uom_qty * (catalog.x_producto.x_studio_unidades_por_caja if catalog.x_producto.x_studio_unidades_por_caja > 0 else 1))
+            return quantity_sale / (days_previous_month - number_of_sundays)
+            
+        else:
+            return 0
