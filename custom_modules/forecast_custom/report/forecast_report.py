4954b3e969b615bb6ec9cec10158b8028541bb05
Andres <acastillo@develoop.net>
2020-11-10 18:30:19 -0400

    Signed-off-by: Andres <acastillo@develoop.net>

--- custom_modules/forecast_custom/report/forecast_report.py
+++ custom_modules/forecast_custom/report/forecast_report.py
@@ -27,55 +27,48 @@ class ForecastReport(models.Model):
     @api.model
     def _select(self):
         return '''
-            SELECT
-                sale.id,
-                sale.x_rotacion AS x_rotacion, 
-                sale.x_kg AS x_kg, 
-                sale.x_cajas AS x_cajas,
-                sum(_sale.Cantidad) AS x_unidades, 
-	            sale.x_unidades AS x_forecast,
-                sale.x_unidades / COALESCE(sum(_sale.Cantidad),1) AS x_desviacion,
+            SELECT min(sale.id) AS id,
 
-                sale.x_mes AS x_date, 
-                sale.x_producto AS x_producto, 
-                sale.x_contacto AS x_client
+            (SELECT DATE_PART('days', DATE_TRUNC('month',min(sale.date_order)) + '1 MONTH'::INTERVAL - '1 DAY'::INTERVAL )) /
+            (SELECT COUNT(*) FROM generate_series(DATE_TRUNC('month',MIN(sale.date_order)), (SELECT (date_trunc('month', MIN(sale.date_order)) + interval '1 month' - interval '1 day')::date), '1 day') AS g(mydate) WHERE EXTRACT(DOW FROM mydate) = 0) /
+            (CASE WHEN MIN(contact.x_studio_puntos_de_venta_foods) > 0 THEN MIN(contact.x_studio_puntos_de_venta_foods) ELSE 1 END) AS x_rotacion,
+
+            (CASE WHEN MIN(product_template.x_studio_unidades_por_caja) > 0 THEN 
+            sum(sale_line.product_uom_qty) / MIN(product_template.x_studio_unidades_por_caja)
+            ELSE sum(sale_line.product_uom_qty) END * MIN(product_template.x_studio_peso_umb_gr) / 1000) AS x_kg,
+
+            CASE WHEN MIN(product_template.x_studio_unidades_por_caja) > 0 THEN 
+            sum(sale_line.product_uom_qty) / MIN(product_template.x_studio_unidades_por_caja)
+            ELSE sum(sale_line.product_uom_qty) END AS x_cajas,
+
+            sum(sale_line.product_uom_qty) AS x_unidades,
+            (Select x_unidades From x_forecast_sale Where x_contacto=sale.partner_invoice_id and x_producto=sale_line.product_id and DATE_TRUNC('month',x_mes) = DATE_TRUNC('month',min(sale.date_order)) limit 1) AS x_forecast,
+            (Select x_unidades From x_forecast_sale Where x_contacto=sale.partner_invoice_id and x_producto=sale_line.product_id and DATE_TRUNC('month',x_mes) = DATE_TRUNC('month',min(sale.date_order)) limit 1) / NULLIF(sum(sale_line.product_uom_qty),0) AS x_desviacion,
+            DATE_TRUNC('month',sale.date_order) AS x_date,
+            sale_line.product_id AS x_producto,
+            sale.partner_invoice_id AS x_client
         '''
 
     @api.model
     def _from(self):
         return '''
-            FROM x_forecast_catalog catalog 
-                INNER JOIN x_forecast_sale sale ON catalog.Id = x_forecast_catalog_id
-                LEFT JOIN 
-                (
-                    SELECT sale_order.partner_invoice_id, sale_order.date_order, sale_order_line.product_id, CASE WHEN product_template.x_studio_unidades_por_caja > 0 
-                        THEN sale_order_line.product_uom_qty * product_template.x_studio_unidades_por_caja ELSE sale_order_line.product_uom_qty END AS Cantidad
-                    FROM sale_order INNER JOIN sale_order_line on sale_order.id = sale_order_line.order_id
-                                    INNER JOIN product_product on sale_order_line.product_id = product_product.id
-                                    INNER JOIN product_template on product_product.product_tmpl_id = product_template.id
-                    WHERE sale_order.state='sale'
-                ) as _sale on _sale.product_id = sale.x_producto 
-                and _sale.partner_invoice_id = sale.x_contacto 
-                and _sale.date_order >= (select date_trunc('month', sale.x_mes)) and _sale.date_order < (select (select date_trunc('month', sale.x_mes)) + interval '1 month' * 1)
+            FROM sale_order sale INNER JOIN sale_order_line sale_line ON sale.id = sale_line.order_id
+		     INNER JOIN product_product product on sale_line.product_id = product.id
+		     INNER JOIN product_template on product.product_tmpl_id = product_template.id
+		     INNER JOIN res_partner contact on sale.partner_invoice_id = contact.id
         '''
 
     @api.model
     def _where(self):
         return '''
+        WHERE sale.state = 'sale' and sale_line.product_id in (SELECT DISTINCT x_producto FROM x_forecast_catalog) 
+        and sale.partner_invoice_id in (SELECT DISTINCT x_contacto FROM x_forecast_catalog)
         '''
 
     @api.model
     def _group_by(self):
         return '''
-            GROUP BY 
-                sale.id,
-                sale.x_rotacion, 
-                sale.x_unidades, 
-                sale.x_kg, 
-                sale.x_cajas,
-	            sale.x_mes, 
-                sale.x_producto, 
-                sale.x_contacto
+            GROUP BY DATE_TRUNC('month',sale.date_order), sale_line.product_id, sale.partner_invoice_id
         '''
 
     def init(self):
